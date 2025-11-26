e4c6bf8633c6fd026720e49b70620a786b16a2f3
prathmesh <pc7play.s@gmail.com>
2025-11-26 09:53:22 +0530

    init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router';
+import './App.css'
+import { HomePage } from './pages';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
